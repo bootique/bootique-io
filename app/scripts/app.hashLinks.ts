7fcdf383c717726d31732eb1cf4d93a247cd6603
AbleOne <evgeny.rugalev@gmail.com>
2018-05-02 22:22:56 +0300

    consistent quotes for strings in js tabs -> spaces; deleted commented code;

--- app/scripts/app.hashLinks.ts
+++ app/scripts/app.hashLinks.ts
@@ -1,22 +1,22 @@
 export function applyHashLinks() {
   // Smooth scroll to #links (https://css-tricks.com/snippets/jquery/smooth-scrolling/)
-	$('a[href*="#"]')
-		.not('[href="#"]')
-		.not('[href="#0"]')
-		.on('click', function(event) {
-			if (
-				location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-				location.hostname == this.hostname
-			) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-				if (target.length) {
-					event.preventDefault();
-					$('html, body').animate({
-						scrollTop: target.offset().top 	// - $('#top-nav').height() - 15   // + height of fixed header + padding from it
-																						// no fixed topNav -> no extra height dependencies
-					}, 300);
-				}
-			}
-		});
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .on('click', function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top  // - $('#top-nav').height() - 15   // + height of fixed header + padding from it
+                                            // no fixed topNav -> no extra height dependencies
+          }, 300);
+        }
+      }
+    });
 }
--- app/scripts/app.injectPlayerYTAPI.ts
+++ app/scripts/app.injectPlayerYTAPI.ts
@@ -1,8 +1,8 @@
 // Inject YouTube API
 export function injectYTplayerAPI() {
-	// Inject YouTube API script
-	var tag = document.createElement('script');
-	tag.src = "//www.youtube.com/player_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  // Inject YouTube API script
+  var tag = document.createElement('script');
+  tag.src = "//www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
